4c16031771262031e0dcebcdf1f1e5929c6a3373
wellysonvie <wellysonvie@gmail.com>
2021-01-27 04:05:13 -0300

    fix: ajustes menu

--- js/home.js
+++ js/home.js
@@ -2,3 +2,18 @@
 $('.menu_mobile_icon').on('click', function(){
     $('.menu_mobile_submenu').toggleClass('ds_none');
 });
+
+$('.menu_mobile_submenu').on('click', function(){
+    $(this).addClass('ds_none');
+});
+
+$("body").mouseup(function (e) {
+    const $menu_mobile_submenu = $(".menu_mobile_submenu");
+    const $menu_mobile_icon = $(".menu_mobile_icon");
+
+    if (!$menu_mobile_submenu.is(e.target) && $menu_mobile_submenu.has(e.target).length === 0
+        && !$menu_mobile_icon.is(e.target) && $menu_mobile_icon.has(e.target).length === 0) {
+        if ($menu_mobile_submenu.is(':visible'))
+            $menu_mobile_submenu.addClass('ds_none');
+    }
+});
